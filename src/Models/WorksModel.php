4a2bdec431888a9ca198e0690cd6edb5126230da
Daniel <d.spinardi199@gmail.com>
2021-01-14 00:20:36 -0300

    Create WorksModel

---
+++ src/Models/WorksModel.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Src\Models;
+
+use CoffeeCode\DataLayer\DataLayer;
+
+class Works extends DataLayer
+{
+    public function __construct()
+    {
+        parent::__construct(
+            "works",
+            [
+                "title",
+                "photo"
+            ],
+            "id",
+            true
+        );
+    }
+
+    public function save(): bool
+    {
+        if(
+            !parent::save()
+        )
+            return false;
+
+        return true;
+    }
+}
