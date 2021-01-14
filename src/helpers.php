b50024bbc69d2efa11d778708aeef1b2bb2e6271
Daniel <d.spinardi199@gmail.com>
2021-01-13 23:35:29 -0300

    Create env helper

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,13 @@
 <?php
 
+if (!function_exists('env'))
+{
+    function env (string $config, ?string $defaultValue = null)
+    {
+        return $_ENV[$config] ?? $defaultValue;
+    }
+}
+
 if (!function_exists('response')) {
     function response ($data)
     {
